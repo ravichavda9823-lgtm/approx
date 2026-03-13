197ceed966d2f3d1646ca9ec463ed71649535956
ravichavda9823-lgtm <ravichavda9823@gmail.com>
2026-03-13 11:57:47 +0530

    first commit

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -46,7 +46,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["https://approx.onrender.com","https://occazone.onrender.com/"],
+    origin: ["https://approx.onrender.com","https://occazone.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
