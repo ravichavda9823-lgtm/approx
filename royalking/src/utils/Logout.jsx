51252fc174ad4aea3d2d5440d291846141f18d55
ravichavda9823-lgtm <ravichavda9823@gmail.com>
2026-03-20 09:14:01 +0530

    second commit

--- royalking/src/utils/Logout.jsx
+++ royalking/src/utils/Logout.jsx
@@ -3,8 +3,8 @@ import Cookie from "js-cookie";
 function Logout() {
   try {
     Cookie.remove("token");
+    window.location.reload = false;
     alert("Logout Successfully...");
-    window.location.href = "/#/login";
   } catch (e) {
     console.log(e);
   }
