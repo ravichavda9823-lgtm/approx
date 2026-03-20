578f2a0eac51ad5c30fd5b2376711af2d0a3b95b
ravichavda9823-lgtm <ravichavda9823@gmail.com>
2026-03-20 10:35:57 +0530

    second commit

--- royalking/src/App.jsx
+++ royalking/src/App.jsx
@@ -95,7 +95,7 @@ function App() {
         </Routes>
         <Footer />
       </BrowserRouter>
-      <ToastContainer  position="top-right" autoClose={8000} theme="colored"/>
+      <ToastContainer  position="top-right" autoClose={3000} theme="colored"/>
     </>
   );
 }
