10e2e041d607bc362fdc79c879b11b9ace11ca93
ravichavda9823-lgtm <ravichavda9823@gmail.com>
2026-03-30 11:37:38 +0530

    second commit

--- royalking/src/pages/Hoteldetails.jsx
+++ royalking/src/pages/Hoteldetails.jsx
@@ -122,7 +122,7 @@ function Hoteldetails() {
               <p><strong>City:</strong> {venue.city}</p>
               <p><strong>Hotel Type:</strong> {venue.type}</p>
                  <p><strong>Hotel price:</strong> {venue.price}</p>
-                  <p><strong>Hotel ShortDesc:</strong> {venue.ShortDesc}</p>
+                  <p><strong>Hotel ShortDesc:</strong> {venue.shortdesc}</p>
             </div>
 
             <div className="col-lg-6">
